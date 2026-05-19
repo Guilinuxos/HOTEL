97d9767f14b9101cb814447b1cf80a25b8bc2820
Guilherme Barros <guilhermecassimiro425@gmail.com>
2026-05-19 15:53:18 -0300

    imports

---
+++ app.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from model import *
+
