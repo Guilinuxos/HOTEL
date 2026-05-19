5081ee4726aa5ff9eebe94882cff235114eef2d5
Guilherme Barros <guilhermecassimiro425@gmail.com>
2026-05-19 16:01:34 -0300

    DASHBOARD

--- app.py
+++ app.py
@@ -7,4 +7,26 @@ from model import *
 
 app = FastAPI()
 
-app
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+# ────────────
+# DASHBOARD
+# ────────────
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    hospedes = consulta_hospedes()
+    quartos = consulta_quartos()
+    reservas = consulta_reservas_ativas()  # já filtradas como ativas no SQL
+
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={
+            "total_hospedes": len(hospedes),
+            "total_quartos": len(quartos),
+            "total_reservas": len(reservas)
+        }
+    )
